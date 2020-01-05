2e0d1692fbd8f255e287ea239333880e4f6682da
Ibrahim Almuqrin <ialmoqren@gmail.com>
2020-01-05 16:34:09 +0300

    Get all todos endpoint

--- course-04/project/c4-final-project-starter-code/backend/src/lambda/http/getTodos.ts
+++ course-04/project/c4-final-project-starter-code/backend/src/lambda/http/getTodos.ts
@@ -1,7 +1,25 @@
 import 'source-map-support/register'
 
 import { APIGatewayProxyEvent, APIGatewayProxyResult, APIGatewayProxyHandler } from 'aws-lambda'
+import { DynamoDB } from 'aws-sdk'
 
+const docClient = new DynamoDB.DocumentClient()
+const todosTable = process.env.TODOS_TABLE
 export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
   // TODO: Get all TODO items for a current user
+  console.log('processing event: ', event)
+
+  const result = await docClient.scan({
+    TableName: todosTable
+  }).promise()
+
+  return {
+    statusCode: 200,
+    headers: {
+      'Access-Control-Allow-Origin': '*'
+    },
+    body: JSON.stringify({
+      items: result.Items
+    })
+  }
 }
