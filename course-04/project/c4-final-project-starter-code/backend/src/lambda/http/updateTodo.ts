baa79421e64e75f3a242a9d71b576c24fac4e951
Ibrahim Almuqrin <ialmoqren@gmail.com>
2020-01-05 20:57:02 +0300

    Update todo service

--- course-04/project/c4-final-project-starter-code/backend/src/lambda/http/updateTodo.ts
+++ course-04/project/c4-final-project-starter-code/backend/src/lambda/http/updateTodo.ts
@@ -3,11 +3,35 @@ import 'source-map-support/register'
 import { APIGatewayProxyEvent, APIGatewayProxyHandler, APIGatewayProxyResult } from 'aws-lambda'
 
 import { UpdateTodoRequest } from '../../requests/UpdateTodoRequest'
+import { getUserId } from '../utils'
+import { DynamoDB } from 'aws-sdk'
+
+const docClient = new DynamoDB.DocumentClient()
+const tableName = process.env.TODOS_TABLE
 
 export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
   const todoId = event.pathParameters.todoId
+  const userId = getUserId(event)
   const updatedTodo: UpdateTodoRequest = JSON.parse(event.body)
+  await docClient.update({
+    TableName: tableName,
+    Key: { userId, todoId },
+    ConditionExpression: 'attribute_exists(userId)',
+    UpdateExpression: 'set todoName = :n, dueDate = :d, done = :y',
+    ExpressionAttributeValues: {
+      ':n': updatedTodo.name,
+      ':d': updatedTodo.dueDate,
+      ':y': updatedTodo.done
+    },
+  }).promise()
 
-  // TODO: Update a TODO item with the provided id using values in the "updatedTodo" object
-  return undefined
+  return {
+    statusCode: 204,
+    headers: {
+      'Access-Control-Allow-Origin': '*'
+    },
+    body: JSON.stringify({
+      updatedTodo
+    })
+  }
 }
