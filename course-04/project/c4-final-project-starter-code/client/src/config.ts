b8d28f55efe20c387c476b15bc2309439e5104f1
Ibrahim Almuqrin <ialmoqren@gmail.com>
2020-01-05 20:56:41 +0300

    Update api id
    It got changed after deleting and redeploying the service

--- course-04/project/c4-final-project-starter-code/client/src/config.ts
+++ course-04/project/c4-final-project-starter-code/client/src/config.ts
@@ -1,5 +1,5 @@
 // TODO: Once your application is deployed, copy an API id here so that the frontend could interact with it
-const apiId = 'dzagn2lex5'
+const apiId = '8g6ke9v1d4'
 export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com/dev`
 
 export const authConfig = {
